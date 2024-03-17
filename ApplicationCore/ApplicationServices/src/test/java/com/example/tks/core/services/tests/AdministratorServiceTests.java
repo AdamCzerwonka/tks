43843f58740591d8737ee07d57193bed90517797
Adam Czerwonka <adamczerwonka59@gmail.com>
2024-03-17 21:13:47 +0100

    feat: tests

--- ApplicationCore/ApplicationServices/src/test/java/com/example/tks/core/services/tests/AdministratorServiceTests.java
+++ ApplicationCore/ApplicationServices/src/test/java/com/example/tks/core/services/tests/AdministratorServiceTests.java
@@ -1,5 +1,6 @@
 package com.example.tks.core.services.tests;
 
+import com.example.tks.core.domain.exceptions.NotFoundException;
 import com.example.tks.core.domain.model.Administrator;
 import com.example.tks.core.services.impl.AdministratorServiceImpl;
 import com.example.tks.ports.infrastructure.AdministratorPort;
@@ -13,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.mockito.Mockito.when;
@@ -40,5 +42,55 @@ public class AdministratorServiceTests {
         when(administratorPort.get()).thenReturn(admins);
 
         Assertions.assertEquals(admins.size(), administratorService.get().size());
+    }
+
+    @Test
+    public void getByIdShouldReturnAdministrator() throws NotFoundException {
+        UUID id = UUID.randomUUID();
+        Administrator admin = new Administrator(id, "TestFirstName", "TestLastName", "admin", true, "Password");
+        when(administratorPort.getById(id)).thenReturn(Optional.of(admin));
+
+        Assertions.assertEquals(admin, administratorService.getById(id));
+    }
+
+    @Test
+    public void getByIdShouldThrowNotFoundException() {
+        UUID id = UUID.randomUUID();
+        when(administratorPort.getById(id)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(NotFoundException.class, () -> administratorService.getById(id));
+    }
+
+    @Test
+    public void getByUsernameShouldReturnAdministrator() throws NotFoundException {
+        String username = "admin";
+        Administrator admin = new Administrator(UUID.randomUUID(), "TestFirstName", "TestLastName", username, true, "Password");
+        when(administratorPort.getByLogin(username)).thenReturn(Optional.of(admin));
+
+        Assertions.assertEquals(admin, administratorService.getByLogin(username));
+    }
+
+    @Test
+    public void getByUsernameShouldThrowNotFoundException() {
+        String username = "admin";
+        when(administratorPort.getByLogin(username)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(NotFoundException.class, () -> administratorService.getByLogin(username));
+    }
+
+    @Test
+    public void createShouldReturnAdministrator() throws Exception {
+        Administrator admin = new Administrator(UUID.randomUUID(), "TestFirstName", "TestLastName", "admin", true, "Password");
+        when(administratorPort.create(admin)).thenReturn(admin);
+
+        Assertions.assertEquals(admin, administratorService.create(admin));
+    }
+
+    @Test
+    public void updateShouldReturnAdministrator() throws NotFoundException {
+        Administrator admin = new Administrator(UUID.randomUUID(), "TestFirstName", "TestLastName", "admin", true, "Password");
+        when(administratorPort.update(admin)).thenReturn(admin);
+
+        Assertions.assertEquals(admin, administratorService.update(admin));
     }
 }

7dd224f884f5908ce456179464608ee965dc75df
Adam Czerwonka <adamczerwonka59@gmail.com>
2024-06-06 00:49:53 +0200

    feat: add metrics

--- UserService/Applications/Web/src/main/java/com/example/tks/app/web/PasikApplication.java
+++ UserService/Applications/Web/src/main/java/com/example/tks/app/web/PasikApplication.java
@@ -1,8 +1,12 @@
 package com.example.tks.app.web;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(scanBasePackages = {"com.example.tks.*"})
 public class PasikApplication {
@@ -10,5 +14,4 @@ public class PasikApplication {
     public static void main(String[] args) {
         SpringApplication.run(PasikApplication.class, args);
     }
-
 }

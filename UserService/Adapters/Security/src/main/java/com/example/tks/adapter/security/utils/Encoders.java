15b1a35ee2c6e09fadab643f9d14dfbccc8dd793
Marcel Badek <kriollik@gmail.com>
2024-06-06 21:54:31 +0200

    feat: add jwt with rsa

---
+++ UserService/Adapters/Security/src/main/java/com/example/tks/adapter/security/utils/Encoders.java
@@ -0,0 +1,33 @@
+package com.example.tks.adapter.security.utils;
+
+import com.example.tks.adapter.security.config.RsaKeyProperties;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+import java.util.UUID;
+
+@Configuration
+@RequiredArgsConstructor
+public class Encoders {
+
+   private final RsaKeyProperties rsaKeyProperties;
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeyProperties.publicKey())
+                .privateKey(rsaKeyProperties.privateKey())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwkSource);
+    }
+}
